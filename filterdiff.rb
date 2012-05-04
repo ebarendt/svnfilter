772dc2a59f36464a270fde51775712dac0cd4c67
Eric Barendt <ebarendt@gmail.com>
2012-05-04 15:03:58 -0700

    gets the filename with the regex

--- filterdiff.rb
+++ filterdiff.rb
@@ -1,3 +1,5 @@
+START_FILE_REGEX = /^--- (?<filename>[\w+\/\.]*)\s*\(revision \d+\)/
+
 files_to_accept = [].tap do |files|
   File.open("files.txt").each do |line|
     files << line.strip
@@ -7,10 +9,10 @@ end
 File.open("changes.diff", "r") do |file|
   match = false
   file.each do |line|
-    if line =~ /^--- [\w+\/\.]*\s*\(revision \d+\)/
+    result = START_FILE_REGEX.match(line)
+    if result
       match = false
-      filename = line.split(/\s+/)[1]
-      if files_to_accept.include?(filename)
+      if files_to_accept.include?(result[:filename])
         match = true
       end
     end
