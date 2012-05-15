daa2ff263c992f7c4d4bbf65d18ac75a2b14a32d
Eric Barendt <ebarendt@gmail.com>
2012-05-15 13:17:00 -0700

    Add some command line parsing and a usage message

--- svn-log.rb
+++ svn-log.rb
@@ -1,6 +1,26 @@
+#!/usr/bin/env jruby --1.9 -s
+
 # Filters svn log output for the list of files changed, optionally accepting changes for only some users.
+# Usage:
+#   svn-log.rb -r=revision -users=user1,user2
 
 require 'nokogiri'
+
+def usage
+  puts <<END
+Filters svn log output for the list of files changed, optionally accepting
+changes for only some users and a log message. Changes are filtered by user
+first, then filtered by message.
+
+Usage:
+  svn-log.rb -r=revision -users=user1,user2 -msg="some message filter"
+
+  -r=revision          revision is the revision from which to start querying
+                       SVN
+  -users=user1,user2   (optional) a comma separated list of users to filter for
+  -msg="a message"     (optional) a string used to filter commit messages
+END
+end
 
 def filter_changes_by_users(changes, users)
   changes.select do |logentry|
@@ -31,11 +51,17 @@ def find_changed_files(changes)
   result.sort.uniq
 end
 
-first_revision = 1190
-users = %w(user1 user2)
+first_revision = $r
+users = if $users then $users.split(",") else nil end
+message = $msg
+
+unless first_revision
+  usage
+  exit(1)
+end
 
 xml = Nokogiri::XML.parse(`svn log -r #{first_revision}:HEAD --xml`)
 changes = get_changes(xml)
 filtered = filter_changes_by_users(changes, users)
-#filtered = filter_changes_by_log(changes, message)
+filtered = filter_changes_by_log(filtered, message)
 puts find_changed_files(filtered)
