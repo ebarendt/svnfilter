8092e1a4b3324a094bdefd16e5f525f36ce35336
zszugyi <zoli.szugyi@gmail.com>
2012-05-16 15:20:07 -0700

    Deal with empty lines in the input and recognize file names with dashes.

--- filterdiff.rb
+++ filterdiff.rb
@@ -3,7 +3,7 @@
 #   filterdiff.rb -f=files.txt changes.diff
 #   filterdiff.rb < changes.diff -- Assumes files.txt
 
-@files_to_accept = Set.new(File.open($f || "files.txt").each_line.map &:strip) unless @files_to_accept
+@files_to_accept = (Set.new(File.open($f || "files.txt").each_line.map &:strip) - ['']) unless @files_to_accept
 
-last_filename = $~[:filename] if /^Index:\s+(?<filename>[\w\/.]*)$/
+last_filename = $~[:filename] if /^Index:\s+(?<filename>[\w\/.-]*)$/
 puts $_ if @files_to_accept.include? last_filename
